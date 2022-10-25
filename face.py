cf27ea317e87770ea2eb334f2f693dd8cf019a40
DAVI XIE <davi_xie@hotmail.com>
2022-10-24 23:05:02 -0300

    edit format face.json

--- face.py
+++ face.py
@@ -15,14 +15,19 @@ f_aux = open("cleanedjson/colors_vertices_cleaned.json")
 colors = json.loads(json.dumps(json.load(f_aux)))
 f_aux.close()
 
+list_aux = []
+
 for i in range(len(data_array)):
-    data_array[i][0]["vertice"] = vertices[data_array[i][0]["vertice"]]
-    data_array[i][0]["cor"] = colors[data_array[i][0]["cor"]]
-    data_array[i][1]["vertice"] = vertices[data_array[i][1]["vertice"]]
-    data_array[i][1]["cor"] = colors[data_array[i][1]["cor"]]
-    data_array[i][2]["vertice"] = vertices[data_array[i][2]["vertice"]]
-    data_array[i][2]["cor"] = colors[data_array[i][2]["cor"]]
+    face_obj = {}
+    face_obj["vertices"] = data_array[i]
+    face_obj["vertices"][0]["vertice"] = vertices[data_array[i][0]["vertice"]]
+    face_obj["vertices"][0]["cor"] = colors[data_array[i][0]["cor"]]
+    face_obj["vertices"][1]["vertice"] = vertices[data_array[i][1]["vertice"]]
+    face_obj["vertices"][1]["cor"] = colors[data_array[i][1]["cor"]]
+    face_obj["vertices"][2]["vertice"] = vertices[data_array[i][2]["vertice"]]
+    face_obj["vertices"][2]["cor"] = colors[data_array[i][2]["cor"]]
+    list_aux.append(face_obj)
 
 f_w = open("face/" + cleaned_file, "w")
-f_w.write(json.dumps(data_array))
+f_w.write(json.dumps(list_aux))
 f_w.close()
